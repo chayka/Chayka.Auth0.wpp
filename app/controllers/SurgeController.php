8d2816f4340202c7dcb941254710de041b18f688
Boris Mossounov <borix@tut.by>
2016-10-21 13:45:50 +0300

    Refactoring to provide temp link

--- app/controllers/SurgeController.php
+++ app/controllers/SurgeController.php
@@ -3,6 +3,7 @@
 namespace Chayka\Auth0;
 
 use Chayka\Helpers\LogHelper;
+use Chayka\Helpers\Util;
 use Chayka\WP\MVC\Controller;
 use Chayka\Helpers\InputHelper;
 use Chayka\WP\Helpers\JsonHelper;
@@ -11,24 +12,34 @@ class SurgeController extends Controller{
 
     public function init(){
         /**
-         * Check JWT authentication
-         */
-        if(!JwtHelper::validate()){
-            JsonHelper::respondError(JwtHelper::getMessage());
-        }
-
-        /**
          * Enable JSON params capture
          */
         InputHelper::captureInput();
     }
 
     public function tempLinkAction(){
+        /**
+         * Check JWT authentication
+         */
+        if(!JwtHelper::validate()){
+            JsonHelper::respondError(JwtHelper::getMessage());
+        }
+
         if(in_array('temp-access-link', JwtHelper::getScopes())){
-            $tempLink = strrev(JwtHelper::getClientId());
+            $tempLink = sprintf('%s://%s/api/surge/temp-access/pass/%s',
+                Util::serverProtocol(), Util::serverName(), strrev(JwtHelper::getClientId()));
             JsonHelper::respond($tempLink);
         }
+
         JsonHelper::respondError('Need "temp-access-link" permission to acquire temporary access link');
+    }
+
+    public function tempAccessAction(){
+        $pass = InputHelper::checkParam('pass')->required()->getValue();
+        InputHelper::validateInput(true);
+        $client = strrev($pass);
+
+        echo "Access granted for client $client\n";
     }
 
 }
